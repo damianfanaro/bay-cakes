4eb230f71dbb4d5dc22532df8daafe69e4323470
Damian Fanaro <damianfanaro@gmail.com>
2017-09-03 15:25:12 +0200

    Switch to Spring Boot 2.0 and moving static site to frontend module

---
+++ api-consumer/src/main/java/com/damianfanaro/baycakes/consumer/BayCakesConsumerStarter.java
@@ -0,0 +1,18 @@
+package com.damianfanaro.baycakes.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * TODO: complete with description
+ *
+ * @author dfanaro
+ */
+@SpringBootApplication
+public class BayCakesConsumerStarter {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BayCakesConsumerStarter.class, args);
+    }
+
+}
